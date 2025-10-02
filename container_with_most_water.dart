a48134a2a69df750209718976cca92754ec78f41
Amgad Mahmoud <amgad.mahmoud.abdallah@gmail.com>
2025-10-02 11:42:13 +0300

    Update container_with_most_water.dart

--- container_with_most_water.dart
+++ container_with_most_water.dart
@@ -1,1 +1,24 @@
 
+class Solution {
+  int maxArea(List<int> height) {
+    int leftIndex = 0;
+    int rightIndex = height.length - 1;
+    int waterArea = 0;
+
+    while(leftIndex<rightIndex){
+        int horizontalArea = rightIndex - leftIndex;
+        int verticalArea = height[leftIndex]<height[rightIndex] ? height[leftIndex] : height[rightIndex];
+        int currentArea = horizontalArea * verticalArea;
+        if(currentArea>waterArea){
+            waterArea = currentArea;
+        }
+
+        if(height[leftIndex]<height[rightIndex]){
+            leftIndex++;
+        }else{
+            rightIndex--;
+        }
+    }
+    return waterArea;
+  }
+}
